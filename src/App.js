a3f3df31705d1db2aae047b37742fd1550e9878d
yash-2507 <99003521+yash-2507@users.noreply.github.com>
2022-05-01 10:07:55 +0530

    added Routes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import { useSelector, useDispatch } from "react-redux";
 import { calculateTotals } from "./features/cart/cartSlice";
 import { useEffect } from "react";
 import Modal from "./components/Modal";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
     const { cartItems } = useSelector((store) => store.cart);
     const { isOpen } = useSelector((store) => store.modal);
     const dispatch = useDispatch();
+    <Routes>
+        <Route path="/cart" component={<App.js />} />
+    </Routes>;
     useEffect(() => {
         dispatch(calculateTotals());
     }, [cartItems]);
